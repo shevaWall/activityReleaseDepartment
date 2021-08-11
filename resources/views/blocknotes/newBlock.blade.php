38b28a73cb13a265ce62c39de4c1fd1bed992062
shevaWall <alexa7454@gmail.com>
2021-08-11 13:48:20 +0300

    шапка  во всю ширину экрана

--- resources/views/blocknotes/newBlock.blade.php
+++ resources/views/blocknotes/newBlock.blade.php
@@ -1,4 +1,4 @@
-<div class="row" data-note-id="{{$note->id}}" data-order-id="{{$note->order_id}}" draggable="true">
+<div class="row pointer" data-note-id="{{$note->id}}" data-order-id="{{$note->order_id}}" draggable="true">
     <div class="col-12 bg-note my-1">
         <div class="note-text p-2">
             <span class="deleteNote" onclick="ajaxDeleteNote({{$note->id}}, '{{csrf_token()}}')">X</span>
